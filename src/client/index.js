b0c7874155c31c5fc054dcb5b8356c6c6fa4acd7
Prateek Gogia <gogia.prateek@hotmail.com>
2019-07-05 00:27:14 +0530

    Added Bootstrap and removed junk from header

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import './scss/app.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
