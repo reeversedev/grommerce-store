7386bd22a71dbf85edd3d71cd18c2a17df2031ee
Prateek Gogia <gogia.prateek@hotmail.com>
2020-02-10 22:55:45 +0530

    Collapsible is ready

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink, Collapse } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const CategoryList = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <div className="bg-white border">
       <p className="px-4 py-4 mb-0">Grocery & Staples</p>
-      <Nav vertical>
-        <NavItem className="border">
-          <NavLink href="#" className="py-3">
-            <FontAwesomeIcon icon={faPlus} color="orange" /> Pulses
+      <Nav vertical className="category-list">
+        <NavItem className="border category-list-item">
+          <NavLink onClick={() => setIsOpen(!isOpen)} className="py-3">
+            <FontAwesomeIcon icon={isOpen ? faMinus : faPlus} color="orange" />{' '}
+            Pulses
           </NavLink>
+          <Collapse isOpen={isOpen}>
+            <div className="border py-3">
+              <span className="ml-5">Masoor</span>
+            </div>
+            <div className="border py-3">
+              <span className="ml-5">Soya</span>
+            </div>
+            <div className="border py-3">
+              <span className="ml-5">Dried Peas & Others</span>
+            </div>
+          </Collapse>
         </NavItem>
         <NavItem className="border">
           <NavLink href="#" className="py-3">
