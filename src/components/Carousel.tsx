142d94565054cce13ef380b0da657bd7ac0150f2
Prateek Gogia <gogia.prateek@hotmail.com>
2020-02-18 21:13:17 +0530

    Carousel is responsive now

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -13,7 +13,33 @@ const Carousel: React.FC<CarouselProps> = props => {
     slidesToShow: 4,
     slidesToScroll: 3,
     arrows: true,
-    touchMove: true
+    touchMove: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return <Slider {...settings}>{props.children}</Slider>;
 };
